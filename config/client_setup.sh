996b66c95e11eac2fdff17950379b6d766e81c21
Kimbsy <lordkimber@gmail.com>
2016-09-11 01:27:23 +0100

    various config files

---
+++ config/client_setup.sh
@@ -0,0 +1,7 @@
+#******************************************************************************#
+# Setup script for client nodes.
+#******************************************************************************#
+
+sudo apt-get update &&
+sudo apt-get upgrade -y &&
+sudo apt-get install -y nmap htop tree vim
